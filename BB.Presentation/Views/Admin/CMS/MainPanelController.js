38f6d0302c9a821b184d81747916b60f6e932de0
Razvan <razvan.cristurean@emerson.com>
2017-04-19 22:53:30 +0300

    Structure

---
+++ BB.Presentation/Views/Admin/CMS/MainPanelController.js
@@ -0,0 +1,35 @@
+﻿var leftNavigation = angular.module("leftNavigation", ['ngRoute']);
+
+leftNavigation.config(function ($routeProvider) {
+    $routeProvider
+        .when("/",
+        {
+            templateUrl: '/Views/Admin/CMS/leftPanel/addPages/Page.html',
+            controller: 'PageController'
+        })
+        .otherwise({
+            redirectTo: "/"
+        });
+});
+
+leftNavigation.controller('PageController', function ($scope) {
+    $scope.pagename = "pages pages";
+});
+
+var mainNavigation = angular.module("mainNavigation", ['ngRoute']);
+
+mainNavigation.config(function ($routeProvider) {
+    $routeProvider
+        .when("/",
+        {
+            templateUrl: '/Views/Admin/CMS/leftPanel/addPages/Page.html',
+            controller: 'PageController'
+        })
+        .otherwise({
+            redirectTo: "/"
+        });
+});
+
+mainNavigation.controller('PageController', function ($scope) {
+    $scope.pagename1 = "pages pages 123";
+});
