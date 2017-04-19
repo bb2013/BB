93cf3492227b08e691da7338e383e93e15b3ebb9
Razvan <razvan.cristurean@emerson.com>
2017-04-19 10:19:29 +0300

    NewSetUpProject

---
+++ BB.Presentation/DataContext/DBContext/DbInit.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using BB.Presentation.DataContext.InternalUsers;
+
+namespace BB.Presentation.DataContext.DBContext
+{
+    public class DbInit : DbContext
+    {
+        public DbInit() : base("BB"){}
+
+        public DbSet<Customer.CustomersDb> Customers { get; set; }
+        public DbSet<InternalUsersDb> InternalUsers { get; set; }
+    }
+}
---
+++ BBPresentation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BBPresentation.WebApiApplication" Language="C#" %>
