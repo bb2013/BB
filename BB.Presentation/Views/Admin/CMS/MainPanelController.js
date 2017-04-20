849bfd93e87593996e645e923ae1505c05c6c74a
Razvan <razvan.cristurean@emerson.com>
2017-04-20 15:32:16 +0300

    MainLayout

--- BB.Presentation/Views/Admin/CMS/MainPanelController.js
+++ BB.Presentation/Views/Admin/CMS/MainPanelController.js
@@ -1,35 +1,32 @@
-﻿var leftNavigation = angular.module("leftNavigation", ['ngRoute']);
+﻿var routerApp = angular.module('routerApp', ['ui.router']);
 
-leftNavigation.config(function ($routeProvider) {
-    $routeProvider
-        .when("/",
-        {
-            templateUrl: '/Views/Admin/CMS/leftPanel/addPages/Page.html',
-            controller: 'PageController'
-        })
-        .otherwise({
-            redirectTo: "/"
+routerApp.config(function($stateProvider, $urlRouterProvider) {    
+    $urlRouterProvider.otherwise('/cms');
+
+    $stateProvider
+        .state('cms', {
+            url: '/cms',
+            views: {
+                // the main template will be placed here (relatively named)
+                '': { templateUrl: '/Views/Admin/CMS/bodyPanel/bodyPanelIndex.html' },
+
+                // the child views will be defined here (absolutely named)
+                'leftPanel@cms': {
+                    templateUrl: '/Views/Admin/CMS/bodyPanel/leftPanel/LeftPanelIndex.html',
+                    controller: 'scotchController4'
+                },
+
+                // for column two, we'll define a separate controller 
+                'rightPanel@cms': {
+                    templateUrl: '/Views/Admin/CMS/bodyPanel/rightPanel/RightPanelIndex.html',
+                    controller: 'scotchController4'
+                }
+            }
         });
 });
 
-leftNavigation.controller('PageController', function ($scope) {
-    $scope.pagename = "pages pages";
-});
-
-var mainNavigation = angular.module("mainNavigation", ['ngRoute']);
-
-mainNavigation.config(function ($routeProvider) {
-    $routeProvider
-        .when("/",
-        {
-            templateUrl: '/Views/Admin/CMS/leftPanel/addPages/Page.html',
-            controller: 'PageController'
-        })
-        .otherwise({
-            redirectTo: "/"
-        });
-});
-
-mainNavigation.controller('PageController', function ($scope) {
-    $scope.pagename1 = "pages pages 123";
+routerApp.controller('scotchController4', function ($scope) {
+    $scope.left = "left";
+    $scope.main = "main";
+    $scope.right = "right";
 });
